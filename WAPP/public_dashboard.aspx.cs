a8f3a2cf4f114b44a1be402969c0405f2a072594
BOON888 <yapboonsiong888@gmail.com>
2025-10-05 17:47:11 +0800

    Revert "Merge branch 'master' of https://github.com/BOON888/WAPP_y2_sem2"
    This reverts commit 62b6548eb752e69f16d50949efaecbe301e312de, reversing
    changes made to d88fd11b379d8fbaf110f0d553161b5f96d98c8a.

--- WAPP/public_dashboard.aspx.cs
+++ WAPP/public_dashboard.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace WAPP
 {
@@ -9,51 +11,7 @@ namespace WAPP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                LoadAdvertisements();
-            }
-        }
 
-        private void LoadAdvertisements()
-        {
-            string connStr = ConfigurationManager.ConnectionStrings["SeaLearnerConnection"].ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                string query = "SELECT AdContent, AdImage FROM Advertisement WHERE GETDATE() BETWEEN StartDate AND EndDate";
-                SqlCommand cmd = new SqlCommand(query, conn);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-
-                rptAds.DataSource = dt;
-                rptAds.DataBind();
-            }
-
-            string connectionString = ConfigurationManager.ConnectionStrings["SeaLearnerDB"].ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                // Count students
-                SqlCommand cmdStudents = new SqlCommand("SELECT COUNT(*) FROM student", conn);
-                int studentCount = (int)cmdStudents.ExecuteScalar();
-
-                // Count educators
-                SqlCommand cmdEducators = new SqlCommand("SELECT COUNT(*) FROM educator", conn);
-                int educatorCount = (int)cmdEducators.ExecuteScalar();
-
-                // Count courses
-                SqlCommand cmdCourses = new SqlCommand("SELECT COUNT(*) FROM course", conn);
-                int courseCount = (int)cmdCourses.ExecuteScalar();
-
-                // Display in labels
-                lblStudentCount.Text = studentCount.ToString("N0"); // formatted with commas
-                lblEducatorCount.Text = educatorCount.ToString("N0");
-                lblCourseCount.Text = courseCount.ToString("N0");
-            }
         }
     }
 }
